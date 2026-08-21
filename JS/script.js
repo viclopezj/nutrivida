1dd922e35b3b155dae33f7771f5dafaf7afd8811
viclopezj <vic.lopezj@duocuc.cl>
2026-08-21 10:07:33 -0400

    modified:   JS/script.js 	modified:   calculadora.html

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,22 @@
  let pantalla = document.getElementById("pantalla");
+ let numero;
 
  function agregar(valor){
+ if(pantalla.value === "0")
     pantalla.value = valor;
+ else
+    pantalla.value += valor;
+ }
+
+ function limpiar(){
+    pantalla.value = "0";
+ }
+ 
+ function suma(){
+    numero = pantalla.value;
+    limpiar();
+ }
+
+ function calcular(){
+    pantalla.value = Number(pantalla.value) + Number(numero);
  }
