1b5437399c637bd051ffb6bb0036937b98f9cfc2
viclopezj <vic.lopezj@duocuc.cl>
2026-08-26 12:36:34 -0400

    renamed:    JS/script.js -> JS/calculo.js 	new file:   JS/validaform.js 	modified:   calculadora.html     new file:   contacto.html

---
+++ JS/validaform.js
@@ -0,0 +1,12 @@
+let formulario = document.getElementById("contacto") //Es para declarar variables
+//var Es para declarar variable global
+let nombre = document.getElementById("nombre")
+
+formulario.addEventListener("submit", function(event){
+    event.preventDefault();
+    if(!/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/.test(nombre.value)){
+        alert("El nombre debe contener solo letras");
+        nombre.focus();
+        return;
+    }
+});
